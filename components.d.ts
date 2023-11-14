f5f03d9f2d794b3ae8f9cd4097d8bf45cfa3fb80
wangcong <wangcong@paraview.cn>
2023-11-14 15:08:50 +0800

    u

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Card: typeof import('./src/components/Card.vue')['default']
+    VanButton: typeof import('vant/es')['Button']
+    VanCalendar: typeof import('vant/es')['Calendar']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
   }
