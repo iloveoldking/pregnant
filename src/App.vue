49b4d48670edd96564a29513dcd3942fb9ad2a61
wangcong <wangcong@paraview.cn>
2023-11-14 11:27:04 +0800

    更新默认超声孕周

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <img class="gif" src="./assets/cat.webp" alt="" />
   <Card :start-time="activeData.startTime" />
   <van-tabbar v-model="tabbarActive">
-    <van-tabbar-item name="lin_chuang" icon="like-o">临床孕周</van-tabbar-item>
     <van-tabbar-item name="chao_sheng" icon="like-o">超声孕周</van-tabbar-item>
+    <van-tabbar-item name="lin_chuang" icon="like-o">临床孕周</van-tabbar-item>
   </van-tabbar>
 </template>
 
@@ -11,17 +11,17 @@
 import { ref, computed } from 'vue';
 
 const data = {
-  lin_chuang: {
-    title: '临床孕周',
-    startTime: '2023-07-27',
-  },
   chao_sheng: {
     title: '超声孕周',
     startTime: '2023-07-21',
   },
+  lin_chuang: {
+    title: '临床孕周',
+    startTime: '2023-07-27',
+  },
 };
 
-const tabbarActive = ref<'lin_chuang' | 'chao_sheng'>('lin_chuang');
+const tabbarActive = ref<'lin_chuang' | 'chao_sheng'>('chao_sheng');
 const activeData = computed(() => data[tabbarActive.value]);
 </script>
 
